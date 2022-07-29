7e2f6447dcda2fbc6396d75671e6c9687d8ba4c9
Robert Michalski <rmicha@softserveinc.com>
2022-07-29 05:52:50 -0700

    use GIVEN THEN pattern in tests

--- src/test/java/Model/DefenderTests.java
+++ src/test/java/Model/DefenderTests.java
@@ -27,6 +27,7 @@ public class DefenderTests {
     @Test
     @DisplayName("Smoke show test to check if code works")
     void smokeShow() {
+        //GIVEN
         var chuck = new Warrior();
         var bruce = new Warrior();
         var carl = new Knight();
@@ -49,6 +50,8 @@ public class DefenderTests {
 
         var army4 = new Army()
                 .addUnits("Warrior", 2);
+
+        //THEN
         Assertions.assertAll(
                 () -> Assertions.assertTrue(Battle.fight(chuck, bruce)),
                 () -> Assertions.assertFalse(Battle.fight(dave, carl)),
@@ -73,8 +76,10 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Warrior and a Model.Knight then Model.Warrior loses")
     void test01() {
+        //GIVEN
         var carl = new Warrior();
         var jim = new Knight();
+        //THEN
         var result = Battle.fight(carl, jim);
         Assertions.assertFalse(result);
     }
@@ -82,8 +87,10 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Knight and a Model.Warrior then Model.Knight wins")
     void test02() {
+        //GIVEN
         var ramon = new Knight();
         var slevin = new Warrior();
+        //THEN
         var result = Battle.fight(ramon, slevin);
         Assertions.assertTrue(result);
     }
@@ -91,8 +98,10 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Warrior and a Model.Warrior then first Model.Warrior to attack survives")
     void test03() {
+        //GIVEN
         var bob = new Warrior();
         var mars = new Warrior();
+        //THEN
         Battle.fight(bob, mars);
         var result = bob.isAlive();
         Assertions.assertTrue(result);
@@ -101,8 +110,10 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Knight and a Model.Warrior then Model.Knight survives")
     void test04() {
+        //GIVEN
         var zeus = new Knight();
         var godkiller = new Warrior();
+        //THEN
         Battle.fight(zeus, godkiller);
         var result = zeus.isAlive();
         Assertions.assertTrue(result);
@@ -111,8 +122,10 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Warrior and a Model.Warrior then second to attack is dead")
     void test05() {
+        //GIVEN
         var husband = new Warrior();
         var wife = new Warrior();
+        //THEN
         Battle.fight(husband, wife);
         var result = wife.isAlive();
         Assertions.assertFalse(result);
@@ -121,8 +134,10 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Warrior and a Model.Knight then knight survives")
     void test06() {
+        //GIVEN
         var dragon = new Warrior();
         var knight = new Knight();
+        //THEN
         Battle.fight(dragon, knight);
         var result = knight.isAlive();
         Assertions.assertTrue(result);
@@ -131,9 +146,11 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Warrior and a Model.Knight then this Model.Knight and new Model.Warrior then new Model.Warrior wins")
     void test07() {
+        //GIVEN
         var unit1 = new Warrior();
         var unit2 = new Knight();
         var unit3 = new Warrior();
+        //THEN
         Battle.fight(unit1, unit2);
         var result = Battle.fight(unit2, unit3);
         Assertions.assertFalse(result);
@@ -142,11 +159,13 @@ public class DefenderTests {
     @Test
     @DisplayName("Given battle between a Model.Defender and a Model.Rookie then the Model.Defender' health is 60")
     void test08() {
+        //GIVEN
         var unit1 = new Defender();
         var unit2 = new Rookie();
+        var expectedHealth = 60;
+        //THEN
         Battle.fight(unit1, unit2);
         var result = unit1.getHealth();
-        var expectedHealth = 60;
         Assertions.assertSame(expectedHealth, result);
     }
 
@@ -154,9 +173,11 @@ public class DefenderTests {
     @DisplayName("Given battle between a Model.Defender and a Model.Rookie then a battle between the Model.Rookie and a new Model.Warrior" +
             "then Model.Defender wins all")
     void test09() {
+        //GIVEN
         var unit1 = new Defender();
         var unit2 = new Rookie();
         var unit3 = new Warrior();
+        //THEN
         Battle.fight(unit1, unit2);
         var result = Battle.fight(unit1, unit3);
         Assertions.assertTrue(result);
@@ -166,10 +187,12 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 1 Model.Warrior unit and second with 2 Model.Warrior units then" +
             " first army loses")
     void test10() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 1);
         var army2 = new Army()
                 .addUnits("Warrior", 2);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertFalse(result);
 
@@ -179,10 +202,12 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one wtih 2 Model.Warrior units and second with 3 Model.Warrior units" +
             "then first army loses")
     void test11() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 2);
         var army2 = new Army()
                 .addUnits("Warrior", 3);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertFalse(result);
     }
@@ -191,10 +216,12 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 5 Model.Warrior units and second with 7 Model.Warrior units" +
             "then first army loses")
     void test12() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 5);
         var army2 = new Army()
                 .addUnits("Warrior", 7);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertFalse(result);
     }
@@ -203,10 +230,12 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 20 Model.Warrior units and second with 21 Model.Warrior units" +
             "then first army wins")
     void test13() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 20);
         var army2 = new Army()
                 .addUnits("Warrior", 21);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertTrue(result);
     }
@@ -215,10 +244,12 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 10 Model.Warrior units and second with 11 Model.Warrior units" +
             "then first army wins")
     void test14() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 10);
         var army2 = new Army()
                 .addUnits("Warrior", 11);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertTrue(result);
     }
@@ -227,10 +258,12 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 11 Model.Warrior units and second with 7 Model.Warrior units" +
             "then first army wins")
     void test15() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 11);
         var army2 = new Army()
                 .addUnits("Warrior", 7);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertTrue(result);
     }
@@ -239,12 +272,14 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 5 Warriors, 4 Defenders, 5 Defenders and second " +
             "with 4 Model.Warrior units then first army wins")
     void test16() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 5)
                 .addUnits("Defender", 4)
                 .addUnits("Defender", 5);
         var army2 = new Army()
                 .addUnits("Warrior", 4);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertTrue(result);
     }
@@ -253,6 +288,7 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 5 Defenders, 20 Warriors, 4 Defenders and second" +
             "with 21 Warriors then first army wins")
     void test17() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Defender", 5)
                 .addUnits("Warrior", 20);
@@ -260,7 +296,7 @@ public class DefenderTests {
                 .addUnits("Warrior", 21);
 
         army1.addUnits("Defender", 4);
-
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertTrue(result);
     }
@@ -269,6 +305,7 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 10 Warriors, 5 Defenders, 10 Defenders and second" +
             "with 5 Warriors then first army wins")
     void test18() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Warrior", 10)
                 .addUnits("Defender", 5);
@@ -276,7 +313,7 @@ public class DefenderTests {
                 .addUnits("Warrior", 5);
 
         army1.addUnits("Defender", 10);
-
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertTrue(result);
     }
@@ -285,12 +322,14 @@ public class DefenderTests {
     @DisplayName("Given battle of two armies one with 2 Defenders, 1 Model.Warrior and 1 Model.Defender and second" +
             "with 5 Warriors then first army loses")
     void test19() {
+        //GIVEN
         var army1 = new Army()
                 .addUnits("Defender", 2)
                 .addUnits("Warrior", 1)
                 .addUnits("Defender", 1);
         var army2 = new Army()
                 .addUnits("Warrior", 5);
+        //THEN
         var result = Battle.fight(army1, army2);
         Assertions.assertFalse(result);
     }
