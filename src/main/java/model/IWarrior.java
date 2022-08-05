478d35070065c711add0d72ab8ac6cca6642f822
Robert Michalski <rmicha@softserveinc.com>
2022-08-05 05:45:55 -0700

    refactor isAlive to ternary operator

--- src/main/java/model/IWarrior.java
+++ src/main/java/model/IWarrior.java
@@ -4,14 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-
 public interface IWarrior extends HasHealth, CanAttack {
     Logger logger = LoggerFactory.getLogger(IWarrior.class);
+
     public abstract void process(Warrior warrior);
+
     default void hit(Warrior opponent) {
         logger.info("{} hits {}", this, opponent);
         opponent.receiveHit(this.getAttack());
     }
+
     default void receiveHit(int damage) {
         reduceHealthBasedOnDamage(damage);
         logger.info("{} took {} damage", this, damage);
@@ -22,11 +24,7 @@ interface HasHealth {
     int getHealth();
 
     default boolean isAlive() {
-        if (getHealth() > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return getHealth() > 0 ? true : false;
     }
 
     void reduceHealthBasedOnDamage(int damage);
